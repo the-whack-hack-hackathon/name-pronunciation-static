edeb42561cfc5b07aa4c19cfe554b0b887016919
srikanth <srikanth.bobbasani@gmail.com>
2022-05-10 21:05:54 +0530

    header bar added

--- src/components/Pronunciation.vue
+++ src/components/Pronunciation.vue
@@ -1,30 +1,39 @@
 <template>
-  <v-container>
-    <v-card class="mt-6 pa-6">
-      <h4 class="text-center">
-        Name Pronunciation Tool 
-      </h4>
-      <p class="font-italic">
-        Everyone has a name. We'll help you pronounce them.
-      </p>
-      <v-form>
+  <div id="app">
+    <v-app id="inspire">
+      <v-card class="overflow-hidden">
+        <v-app-bar app  elevate-on-scroll scroll-target="#scrolling-techniques-7" elevation="12">
+          <v-toolbar-title class="flex">Name Pronunciation Tool</v-toolbar-title>
+        </v-app-bar>
+        <v-sheet id="scrolling-techniques-7" class="overflow-y-auto" max-height="60" height="60">
+          
+        </v-sheet>
+      </v-card>
+      <v-main>
         <v-container>
-          <v-row>
-            <v-col cols="12" sm="6" md="3">
-              <v-text-field solo label="Name" v-model="txtName">
-              </v-text-field>
-              <v-btn color="primary" @click="requestSpokenName">
-                <v-icon left>
-                  mdi-play-circle
-                </v-icon>
-                Listen
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-    </v-card>
-  </v-container>
+            <v-card>
+              <v-form>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="3">
+                      <v-text-field solo label="Name" v-model="txtName">
+                      </v-text-field>
+                      <v-btn color="primary" @click="requestSpokenName">
+                        <v-icon left>
+                          mdi-play-circle
+                        </v-icon>
+                        Listen
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+            </v-card>
+          </v-container>
+      </v-main>
+    </v-app>
+  </div>
+
 </template>
 
 <script lang='ts'>
@@ -32,9 +41,10 @@ import { Vue } from "vue-class-component";
 
 export default class Pronunciation extends Vue {
   public txtName = "";
-  
+
   public requestSpokenName() {
     console.log(`Your name is ${this.txtName}`);
+    // validation if name is empty
     var audio: any = new Audio("/static/service-bell_daniel_simion.mp3");
     audio.play();
   }
