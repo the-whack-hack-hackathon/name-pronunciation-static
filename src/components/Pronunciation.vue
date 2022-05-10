c2f8fa2adccbd64db295ab7d6cce58aaa288a3a8
William Curtis <billc@targoe.local>
2022-05-09 22:21:36 -0400

    Initial Load

---
+++ src/components/Pronunciation.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <v-card class="mt-6 pa-6">
+      <h4 class="text-center">
+        Name Pronunciation Tool 
+      </h4>
+      <p class="font-italic">
+        Everyone has a name. We'll help you pronounce them.
+      </p>
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field solo label="Name" v-model="txtName">
+              </v-text-field>
+              <v-btn color="primary" @click="requestSpokenName">
+                <v-icon left>
+                  mdi-play-circle
+                </v-icon>
+                Listen
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang='ts'>
+import { Vue } from "vue-class-component";
+
+export default class Pronunciation extends Vue {
+  public txtName = "";
+  
+  public requestSpokenName() {
+    console.log(`Your name is ${this.txtName}`);
+    var audio: any = new Audio("/static/service-bell_daniel_simion.mp3");
+    audio.play();
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <pronunciation />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Pronunciation from '../components/Pronunciation.vue';
+
+// Components
+import HelloWorld from '../components/Pronunciation.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    Pronunciation
+  },
+});
+</script>
